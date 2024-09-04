8cb649da2dd142b27bbbb19955a3b4fed8820e11
Harshit Manchanda <128061019+harshit6239@users.noreply.github.com>
2024-09-04 20:15:41 +0530

    build

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3000'
-    }
-  },
-  plugins: [react()],
-})
+    server: {
+        proxy: {
+            "/api": "https://yt-stud-bud-backend.onrender.com/api",
+        },
+    },
+    plugins: [react()],
+});
